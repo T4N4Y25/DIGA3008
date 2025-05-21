69a649e5324c6535a9375f5dfe7062160fb1bb32
Tanay <tanayrangasamy@gmail.com>
2025-05-22 00:19:25 +0200

    Finished Design page JavaScript

--- Scripts/bloghome.js
+++ Scripts/bloghome.js
@@ -1,6 +1,7 @@
 const blogcards = [
+  //An array with relevant details for the blog card (the link to the blog page, the title week and the blog title)
   {
-    num: 1,
+    num: 1, //Simply helps track blogs in the array
     link: "Blog1.html",
     Week: "Week 1 Blog",
     title: "Proposal for a Universal Electronic Publishing System and Archive",
@@ -39,16 +40,17 @@ const blogcards = [
   },
 ];
 
-let blogcontainer = document.querySelector(".u-blogsnav");
-let cardsperpage = 3;
-let index = 0;
+let blogcontainer = document.querySelector(".u-blogsnav"); //Main container representing a blog card
+let cardsperpage = 3; //constant variable representing the max number of blog cards on the page at a time
+let index = 0; //Index used to access the blogs stored in the array
 
 function DisplayBlogs(bloglist) {
-  let visibleblogs = bloglist.slice(index, index + cardsperpage);
+  let visibleblogs = bloglist.slice(index, index + cardsperpage); //Cuts the array to get the blog at the current index and the subsequent two blogs
   let displayblog = visibleblogs
     .map(function (visibleblogs) {
+      //Use the map function similarly to the exercises done in class to automatically add html to the blogshome.html file with the blog card info from the array
       return `            <li>
-              <a class="u-blogsnav" href="${visibleblogs.link}">
+              <a class="u-blogsnav" href="${visibleblogs.link}">    
                 <div class="u-blogcard">
                   <p class="p-blogweek">${visibleblogs.Week}</p>
                   <p class="p-blogtitle">
@@ -57,39 +59,41 @@ function DisplayBlogs(bloglist) {
                 </div>
               </a>
             </li>`;
-    })
+    }) //The html structure initially used in the blogshome.html file, JavaScript allows this structure to be repeated making adding additional blog cards easier
     .join(" ");
-  blogcontainer.innerHTML = displayblog;
-  
+  blogcontainer.innerHTML = displayblog; //Write to the html file
 }
 
-DisplayBlogs(blogcards);
+DisplayBlogs(blogcards); //Display 3 initial blogs when index is at 0
 
-let buttonnext = document
+let buttonnext = document //Forward button from html file
   .querySelector(".next-btn")
   .addEventListener("click", NextBlog);
 
-  let buttonprev = document.querySelector(".prev-btn").addEventListener("click",PrevBlog);
+let buttonprev = document
+  .querySelector(".prev-btn")
+  .addEventListener("click", PrevBlog); //Back button from html file
 function NextBlog() {
+  //function to display the next three blog cards when the forward button is clicked
   // index+=cardsperpage;
-  index += cardsperpage - 1;
-  if (index > blogcards.length-1) {
+  index += cardsperpage - 1; //Index is updated to the next blog card after the third one initially displayed
+  if (index > blogcards.length - 1) {
+    //Checks if the index exceeds the array length and resets the index back to 0 if it does
     index = 0;
   }
-  
-  DisplayBlogs(blogcards);
+
+  DisplayBlogs(blogcards); //Display the next three blogs after index is updated
   // alert("Button was clicked");
 }
 
-function PrevBlog(){
-  index -= (cardsperpage);
-  if(index < 0){
-    let maxPages = Math.floor(blogcards.length/cardsperpage);
-    index = blogcards.length-1;
+function PrevBlog() {
+  //function to display the previous three blog cards
+  index -= cardsperpage; //Set index value to the value just before the first blog currently displayed
+  if (index < 0) {
+    //Check index is not a negative value
+    //let maxPages = Math.floor(blogcards.length/cardsperpage); //A method I am trying so I can fix a bug where the first week blog is never showing when moving backwards
+    index = blogcards.length - 1; //Cycle the index to the last blog card
   }
-    if (index >= blogcards.length) {
-      index = Math.max(0, blogcards.length - cardsperpage);
-    }
-  
+
   DisplayBlogs(blogcards);
 }
---
+++ Scripts/design.js
@@ -0,0 +1,67 @@
+let linkList = document.querySelectorAll(".QuickLinkList a"); //store all the side panel links
+
+let arrLinks = Array.from(linkList); //convert the links to an array for easier manipulation
+
+let arrSections = [ //An array storing names matching the data attribute of the links and the relevant section related to those links
+  { name: "DesignIxD", section: document.querySelector(".DesignIxD") }, //For example the data attribute here is called DesignIxD and the section that displays when this link is clicked is also DesignIxD
+  {
+    name: "StyleProcess",
+    section: document.querySelector(".StyleProcess"),
+  },
+  {
+    name: "StyleGuide",
+    section: document.querySelector(".StyleGuide"),
+  },
+  {
+    name: "WireframeSection",
+    section: document.querySelector(".WireframeSection"),
+  },
+  {
+    name: "References",
+    section: document.querySelector(".References"),
+  },
+];
+
+SetColour(arrLinks); //Set all the colours of the link to it's 'unselected' colour
+Initialize(); //Set the default section to IxD process when the page initially loads
+
+arrLinks.forEach((link) => { //Iterate through the array with all the links
+  let linkName = ""; //a variable to store the data attribute of the selected link
+  link.addEventListener("click", function (e) { //Perfroms the subsequent code when the link is clicked, the e in the function declaration is to disable the auto-scrolling I initially had in HTML
+    SetColour(arrLinks); //reset all the links to 'unselected' colour 
+
+    link.classList.toggle("highlight", true); //Make only the selected link the selected colour
+    e.preventDefault(); //Prevents the auto-scrolling functionality I initially had in HTML
+    window.scrollTo({ //When a link is clicked, the page auto-scrolls to the top so the user can read from the beginning of each section
+      top: 100,
+      behavior: "smooth",
+    });
+    linkName = link.dataset.filter; //variable stores the data attribute of the clicked link
+    arrSections.forEach((elem) => { //a nested loop to now iterate the array with all the sections and compare them with the stored data attribute
+      if (linkName === "") { //Incase of errors
+        return;
+      }
+      if (elem.name === linkName) { //Compare the elements name property with the data attribute of the clicked link
+        // alert(elem.name); 
+        elem.section.classList.toggle("visible", true); //If a match is found that section is displayed
+      } else {
+        elem.section.classList.toggle("visible", false); //If the element does not match the section is not visible
+      }
+    });
+  });
+});
+
+function SetColour(arr) { //Function to set the colour of the links to their default colour
+  arr.forEach((colour) => {
+    colour.classList.toggle("highlight", false);
+  });
+}
+
+function Initialize(){ //Function to set the default section shown when the page loads
+  arrSections.forEach((val) =>{
+    val.section.classList.toggle("visible",false); //Make every other section not visible
+  })
+
+  arrSections[0].section.classList.toggle("visible",true); //Make ony the first section true
+  arrLinks[0].classList.toggle("highlight",true); //Set the 'selected' colour for the corresponding link
+}
