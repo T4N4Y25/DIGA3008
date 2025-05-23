a4c132f5045160a7d1a98569234c8ee39f8f9c93
Tanay <tanayrangasamy@gmail.com>
2025-05-23 22:05:37 +0200

    Tried to tweak hamburger menu but will continue working on it later

--- Scripts/nav.js
+++ Scripts/nav.js
@@ -2,9 +2,8 @@ let navList = document.querySelector(".u-navbarlist"); //class for the navigatio
 
 let logoimage = document
   .querySelector(".logo")
-  .addEventListener("click", displayLinks); //When the corresponding image is clicked the links are displayer or hidden
-
+  .addEventListener("click", displayLinks);
 function displayLinks() {
   navList.classList.toggle("hide"); //A hide class in the styles.css file moves the links in and out of view using transition
-  //alert("logo clicked");           for debugging
+  //alert("logo clicked");           For debugging
 }
