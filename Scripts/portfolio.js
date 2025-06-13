4b2360eb483bb4a9d0d769ad412a78006d593db5
Tanay <tanayrangasamy@gmail.com>
2025-06-13 20:10:37 +0200

    Updated Portfolio JS

--- Scripts/portfolio.js
+++ Scripts/portfolio.js
@@ -1,3 +1,4 @@
+//API
 const username = "T4N4Y25"; // My GitHub Username
 const repoList = document.getElementById("repo-list"); //Get the list where the repositories will be added
 
@@ -16,3 +17,55 @@ fetch(`https://api.github.com/users/${username}/repos`) //Uses GitHub Rest API
   .catch((error) => {
     console.error("Error fetching repos:", error); //If files could not be loaded, create an error message
   });
+
+//Scrolling functionality
+
+let Games = [
+  document.querySelector('.p-GamesTitle'),
+  document.querySelector(".firstGame"),
+  document.querySelector(".secondGame"),
+  document.querySelector(".thirdGame"),
+];
+
+let Eng = [
+  document.querySelector('.p-EngTitle'),
+  document.querySelector(".firstEng"),
+  document.querySelector(".secondEng"),
+  document.querySelector(".thirdEng"),
+];
+
+let GamesPos = [
+  document.getElementById("Games"),
+  document.getElementById("AirHockey"),
+  document.getElementById("Inventory"),
+  document.getElementById("Winter"),
+];
+
+let EngPos = [
+  document.querySelector('.p-EngTitleTwo'),
+  document.getElementById("Micros"),
+  document.getElementById("SoftwareDev"),
+  document.getElementById("ElecPrj"),
+]
+
+Games.forEach((current, i) =>{
+  
+  current.addEventListener("click", function(e){
+    e.preventDefault();
+
+    window.scrollTo({
+      top: GamesPos[i].offsetTop,
+      behavior: "smooth"
+    })
+  })
+})
+
+Eng.forEach((current,i) =>{
+  current.addEventListener("click", () => {
+    window.scrollTo({
+      top: EngPos[i].offsetTop,
+      behavior: "smooth"
+    })
+  })
+})
+
